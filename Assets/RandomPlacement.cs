ebae09bbc51816c8c3d4ab1f91d8cf59a9a34696
Simon <Simon.hunt@solihull.ac.uk>
2020-08-25 14:18:07 +0100

    Revert "Cleanup Extra Files"
    This reverts commit c2f7b8caf511e66ab16d14a51411c86c1635dea0.

---
+++ Assets/RandomPlacement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPlacement : MonoBehaviour
+{
+    int placed = 0;
+    public GameObject[] objectsToPlace;
+
+    public GameObject sphere;
+
+    void Start()
+    {
+        objectsToPlace = new GameObject[transform.childCount];
+        for (int i=0;i<transform.childCount;i++)
+        {
+            objectsToPlace[i] = transform.GetChild(i).gameObject;
+        }
+        RandomGeneration();
+    }
+
+    public void RandomGeneration()
+    {
+        Vector3 NewLocation;
+        bool isInside;
+
+        GameObject objectToPlace;
+
+        for (int i = 0; i < objectsToPlace.Length; i++)
+        {
+            objectToPlace = objectsToPlace[i];
+            NewLocation = (Random.insideUnitSphere)*500;
+            isInside = Physics.CheckSphere(NewLocation, objectToPlace.transform.GetComponent<SphereCollider>().radius);
+            
+            while(isInside)
+            {
+                NewLocation = (Random.insideUnitSphere * 500);
+                isInside = Physics.CheckSphere(NewLocation, objectToPlace.transform.GetComponent<SphereCollider>().radius);
+            }
+            objectToPlace.transform.position = NewLocation;
+
+        }
+        sphere.SetActive(true);
+
+
+    }
+
+
+}
