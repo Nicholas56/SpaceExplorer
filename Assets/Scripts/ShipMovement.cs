df3589c63115f57f3a4f2c3058299f05541bc2dc
Nicholas56 <45455888+Nicholas56@users.noreply.github.com>
2020-10-13 22:03:14 +0100

    CrossPlatform stuff
    Added crossplatform support
    Added probe launch

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -8,6 +8,49 @@ public class ShipMovement : MonoBehaviour
 {
     Rigidbody rb;
     [SerializeField]float speed=5;
+    PlayerControls controls;
+
+    [SerializeField] GameObject probe;
+    GameObject currentProbe;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        Quaternion deltaRotation =
+            Quaternion.Euler(new Vector3(-value.Get<Vector2>().y, value.Get<Vector2>().x) * (Time.deltaTime * 10));
+
+        rb.MoveRotation(rb.rotation * deltaRotation);
+
+        rb.velocity = transform.forward * speed;
+    } 
+
+    public void OnFire(InputValue value)
+    {
+        DestroyProbe();
+        GameObject newProbe = Instantiate(probe, transform.position - Vector3.down, transform.rotation);
+        newProbe.GetComponent<Rigidbody>().AddForce(transform.forward * 25, ForceMode.Impulse);
+
+        Invoke("DestroyProbe", 5f);
+    }
+
+    void DestroyProbe()
+    {
+        Destroy(currentProbe);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +64,14 @@ public class ShipMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        /*
         Quaternion deltaRotation = 
-        Quaternion.Euler(new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"))* Time.deltaTime * 40);
+        Quaternion.Euler(new Vector3(CrossPlatformInputManager.GetAxis("Vertical"), CrossPlatformInputManager.GetAxis("Horizontal"))* Time.deltaTime * 40);
 
         rb.MoveRotation(rb.rotation * deltaRotation);
         rb.velocity = transform.forward*speed;
-
+        */
         if (Input.GetKeyDown(KeyCode.Space)) {rb.angularVelocity = Vector3.zero; }
+        
     }
 }
